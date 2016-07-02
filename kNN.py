d58d122ae5d5b477a6ccf44d73740fc98cb8636e
Jussi Martin <jussi.h.a.martin@gmail.com>
2016-07-02 18:51:07 +0300

    img2vector

--- kNN.py
+++ kNN.py
@@ -83,3 +83,13 @@ def classifyPerson():
     classfierResult = classify0((inArr
         - minVals) / ranges, normMat, datingLabels, 3)
     print("You will probably like this person: ", classfierResult)
+
+def img2vector(filename):
+    returnVect zeroes((1,1024))
+    fr = open(filename)
+    for i in range(32):
+        lineStr = fr.readline()
+        for j in range(32):
+            returnVect[0, 32*i+j] = int(lineStr[j])
+    return returnVect
+    
