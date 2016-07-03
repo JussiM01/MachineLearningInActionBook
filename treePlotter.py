9e43db6467c7956c8eccd58d3309ed8683831b77
Jussi Martin <jussi.h.a.martin@gmail.com>
2016-07-03 17:34:51 +0300

    fixed a typo

--- treePlotter.py
+++ treePlotter.py
@@ -12,8 +12,7 @@ def plotNode(nodeTxt, centerPt, parentPt, nodeType):
 def createPlot():
     fig = plt.figure(1, facecolor="white")
     fig.clf()
-    createPlot.ax1 = plt.subplot(111, frame=False)
+    createPlot.ax1 = plt.subplot(111, frameon=False)
     plotNode('a decision node', (0.5, 0.1), (0.1, 0.5), decisionNode)
     plotNode('a leaf node', (0.8, 0.1), (0.3, 0.8), leafNode)
     plt.show()
-    
