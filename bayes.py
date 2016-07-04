699f009805e807a293d67eb1a42ea66f3007b791
Jussi Martin <jussi.h.a.martin@gmail.com>
2016-07-04 18:24:34 +0300

    fixed some typos

--- bayes.py
+++ bayes.py
@@ -73,23 +73,23 @@ def bagOfWords2VecMN(vocabList, inputSet):
 def textParse(bigString):
     import re
     listOfTokens = re.split(r'\W*', bigString)
-    return [tok.lower() for tok in listOfTokens len(tok) > 2]
+    return [tok.lower() for tok in listOfTokens if len(tok) > 2]
 
 def spamTest():
     docList = []; classList = []; fullText = []
-    for i in range(26):
+    for i in range(1, 26):
         wordList = textParse(open('email/spam/%d.txt' % i,
-            encoding='iso-8859-1')).read())
+            encoding='iso-8859-1').read())
         docList.append(wordList)
         fullText.append(wordList)
         classList.append(1)
         wordList = textParse(open('email/ham/%d.txt' % i,
-            encoding='iso-8859-1')).read())
+            encoding='iso-8859-1').read())
         docList.append(wordList)
         fullText.append(wordList)
         classList.append(0)
     vocabList = createVocabList(docList)
-    trainingSet = range(50); testSet = []
+    trainingSet = list(range(50)); testSet = []
     for i in range(10):
         randIndex = int(random.uniform(0, len(trainingSet)))
         testSet.append(trainingSet[randIndex])
@@ -98,7 +98,7 @@ def spamTest():
     for docIndex in trainingSet:
         trainMat.append(setOfWords2Vec(vocabList, docList[docIndex]))
         trainClasses.append(classList[docIndex])
-    p0V,P1V, PSpam = trainNB0(array(trainMat), array(trainClasses))
+    p0V,p1V, pSpam = trainNB0(array(trainMat), array(trainClasses))
     errorCount = 0
     for docIndex in testSet:
         wordVector = setOfWords2Vec(vocabList, docList[docIndex])
