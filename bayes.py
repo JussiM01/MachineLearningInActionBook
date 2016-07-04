bed0effd696ae4dc9785b527ae79e37c6a8c3a75
Jussi Martin <jussi.h.a.martin@gmail.com>
2016-07-04 15:54:12 +0300

    added some functions

--- bayes.py
+++ bayes.py
@@ -21,3 +21,21 @@ def setOfWords2Vec(vocabList, inputSet):
             returnVec[vocabList.index(word)] = 1
         else: print("the word: %s is not in my Vocabulary!" % word)
     return returnVec
+
+def trainNB0(trainMatrix, trainCateory):
+    numTrainDocs = len(trainMatrix)
+    numWords = len(trainMatrix[0])
+    pAbusive = sum(trainCateory)/float(numTrainDocs)
+    p0Num = zeros(numWords); p1Num = zeros(numWords)
+    p0Denom = 0.0; p1Denom = 0.0
+    for i in range(numTrainDocs):
+        if trainCateory[i] == 1:
+            p1Num += trainMatrix[i]
+            p1Denom += sum(trainMatrix[i])
+        else:
+            p0Num += trainMatrix[i]
+            p0Denom += sum(trainMatrix[i])
+    p1Vect = p1Num/p1Denom     # change to log()
+    p0Vect = p0Num/p0Denom     # change to log()
+    return p0Vect, p1Vect, pAbusive
+    
