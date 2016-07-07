3d8ae3c260a6a665cc2e65a799ee754f36d0f711
Jussi Martin <jussi.h.a.martin@gmail.com>
2016-07-07 16:44:05 +0300

    fixed a typo

--- svmMLiA.py
+++ svmMLiA.py
@@ -141,7 +141,7 @@ def innerL(i, oS):
             (oS.alphas[j]-alphaJold)*oS.X[i,:]*oS.X[j,:].T
         b2 = oS.b - Ej - oS.labelMat[i]*(oS.alphas[i]-alphaIold)*\
             oS.X[i,:]*oS.X[j,:].T - oS.labelMat[j]*\
-            (oS.alphas[j]-alphaJold)*oS.X[j,:]*oS[j,:].T
+            (oS.alphas[j]-alphaJold)*oS.X[j,:]*oS.X[j,:].T
         if (0 < oS.alphas[i]) and (oS.c > oS.alphas[i]): oS.b = b1
         elif (0 < oS.alphas[j]) and (oS.C > oS.alphas[j]): oS.b = b2
         else: oS.b = (b1 + b2)/2.0
