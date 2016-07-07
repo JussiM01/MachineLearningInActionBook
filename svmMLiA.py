d14c8e5834a0367ec72e17b7d2e0fdd429882bfb
Jussi Martin <jussi.h.a.martin@gmail.com>
2016-07-07 16:20:28 +0300

    added some function

--- svmMLiA.py
+++ svmMLiA.py
@@ -146,4 +146,29 @@ def innerL(i, oS):
         elif (0 < oS.alphas[j]) and (oS.C > oS.alphas[j]): oS.b = b2
         else: oS.b = (b1 + b2)/2.0
         return 1
-    else: return 0 
+    else: return 0
+
+def smoP(dataMat, classLabels, C, toler, maxIter, kTup=('lin', 0)):
+    oS = optStruct(mat(dataMatIn), mat(classLabels).transpose(), C, toler)
+    iter = 0
+    entireSet = True; alphaPairsChanged = 0
+    while (iter < maxIter) and ((alphaPairsChanged > 0) or (entireSet)):
+        alphaPairsChanged = 0
+        if entireSet:
+            for i in range(oS.m):
+                alphaPairsChanged += innerL(i, oS)
+            print("fullSet, iter: %d i:%d, pairs changed %d" %\
+                (iter, i, alphaPairsChanged))
+            iter += 1
+        else:
+            nonBoundIs = nonzero((oS.alphas.A > 0) * (oS.alphas.A <C))[0]
+            for i in nonBoundIs:
+                alphaPairsChanged += innerL(i, oS)
+                print("non-bound, iter: %d i:%d, pairs changed %d" % \
+                (iter, i, alphaPairsChanged))
+            iter += 1
+        if entireSet: entireSet = False
+        elif (alphaPairsChanged): entireSet = True
+        print("iteration number: %d" % iter)
+    return oS.b, oS.alphas
+    
