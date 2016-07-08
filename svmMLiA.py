942ff8f9de271a5bce3c27416a1d916b498fad9f
Jussi Martin <jussi.h.a.martin@gmail.com>
2016-07-08 15:57:49 +0300

    ..

--- svmMLiA.py
+++ svmMLiA.py
@@ -201,3 +201,29 @@ def kernelTrans(X, A, kTup):
     else: raise NameError('Houston We Have a Problem -- \
     That Kernel is not recognized')
     return K
+
+def testRbf(k1=1.3):
+    dataArr, labelArr = loadDataSet('svm/testSetRBF.txt')
+    b, alphas = smoP(dataArr, labelArr, 200, 0,0001, 1000, ('rbf', k1))
+    datMat=mat(dataArr); labelMat = mat(labelArr).transpose()
+    svInd=nonzero(alphas.A>0)[0]
+    sVs=datMat[svInd]
+    labelSV = labelMat[svInd]
+    print("there are %d Support Vectors" % shape(sVs)[0])
+    m, n = shape(datMat)
+    errorCount = 0
+    for i in range(m):
+        KernelEval = kernelTrans(sVs, datMat[i, :], ('rbf', k1))
+        predict=KernelEval.T * multiply(labelSV, alphas[svInd]) + b
+        if sign(predict) != sign(labelArr[i]): errorCount += 1
+    print("the training error rate is: %f" (float(errorCount)/m))
+    dataArr, labelArr = loadDataSet('svm/testSetRBF2.txt')
+    errorCount = 0
+    datMat=mat(dataArr); labelMat = mat(labelArr).transpose()
+    m, n = shape(datMat)
+    for i in range(m):
+        KernelEval = kernelTrans(sVs, datMat[i,:], ('rbf', k1))
+        predict=KernelEval.T * multiply(labelSV, alphas[svInd]) + b
+        if sign(predict) != sign(labelArr[i]): errorCount += 1
+    print("the test error rate is: %f" % (float(errorCount)/m))
+    
