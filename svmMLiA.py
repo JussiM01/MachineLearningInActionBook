e7e23f903de419abbde1f9d0854776863b48361e
Jussi Martin <jussi.h.a.martin@gmail.com>
2016-07-08 16:05:11 +0300

    fixed a typo

--- svmMLiA.py
+++ svmMLiA.py
@@ -204,7 +204,7 @@ def kernelTrans(X, A, kTup):
 
 def testRbf(k1=1.3):
     dataArr, labelArr = loadDataSet('svm/testSetRBF.txt')
-    b, alphas = smoP(dataArr, labelArr, 200, 0,0001, 1000, ('rbf', k1))
+    b, alphas = smoP(dataArr, labelArr, 200, 0.0001, 1000, ('rbf', k1))
     datMat=mat(dataArr); labelMat = mat(labelArr).transpose()
     svInd=nonzero(alphas.A>0)[0]
     sVs=datMat[svInd]
@@ -226,4 +226,3 @@ def testRbf(k1=1.3):
         predict=KernelEval.T * multiply(labelSV, alphas[svInd]) + b
         if sign(predict) != sign(labelArr[i]): errorCount += 1
     print("the test error rate is: %f" % (float(errorCount)/m))
-    
