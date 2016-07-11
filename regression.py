59fcd947a2fbb913a9df08ee768c97aadf355a30
Jussi Martin <jussi.h.a.martin@gmail.com>
2016-07-11 16:48:21 +0300

    ..

--- regression.py
+++ regression.py
@@ -69,4 +69,27 @@ def ridgeTest(xArr, yArr):
         ws = ridgeRegres(xMat, yMat, exp(i-10))
         wMat[i,:] = ws.T
     return wMat
+
+def stageWise(xArr, yArr, eps=0.01, numIt=100):
+    xMat = mat(xArr); yMat = mat(yArr).T
+    yMean = mean(yMat, 0)
+    yMat = yMat - yMean
+    xMat = regularize(xMat)
+    m, n = shape(xMat)
+    ws = zeeros((n, 1)); wsTest = ws.copy(); wsMax = ws.copy()
+    for i in range(numIt):
+        print("ws.T")
+        lowestError = inf
+        for j in range(n):
+            for sign in [-1, 1]:
+                wsTest = ws.copy()
+                wsTest[j] += esp*sign
+                yTest = xMat*wsTest
+                rssE = rssError(yMat, yTest.A)
+                if rssE < lowestError:
+                    lowestError = rssE
+                    wsMax = wsTest
+        ws = wsMax.copy()
+        returnMat[i,:] = ws.T
+    return returnMat
     
