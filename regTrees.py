c0b26e6bd373aa1dfc29722a59ca8376d0d9690c
Jussi Martin <jussi.h.a.martin@gmail.com>
2016-07-11 18:15:50 +0300

    ..

--- regTrees.py
+++ regTrees.py
@@ -30,3 +30,27 @@ def createTree(dataSet, leafType=regLeaf, errType=regErr, ops=(1,4)):
     retTree['left'] = createTree(lSet, leafType, errType, ops)
     retTree['right']= createTree(rSet, leafType, errType, ops)
     return retTree
+
+def chooseBestSplit(dataSet, leafType=regLeaf, errType=regErr, ops(1,4)):
+    tolS = ops[0]; tolN = ops[1]
+    if len(set(dataSet[:,-1].T.tolist()[0])) == 1:
+        return None, leafType(dataSet)
+    m, n = shape(dataSet)
+    S = errType(dataSet)
+    bestS = inf; bestIndex = 0; bestValue = 0
+    for featIndex in range(n-1):
+        for splitVal in set(dataSet[:, featIndex]):
+            mat0, mat1 = binSplitDataSet(dataSet, featIndex, splitVal)
+            if (shape(mat0)[0] < tolN) or (shape(mat1)[0] < tolN): continue
+            newS = errType(mat0) + errType(mat1)
+            if newS < bestS:
+                bestIndex = featIndex
+                bestValue = splitVal
+                bestS = newS
+    if (S - bestS) < tolS:
+        return None, leafType(dataSet)
+    mat0, mat1 = binSplitDataSet(dataSet, bestIndex, bestValue)
+    if (shape(mat0)[0] < tolN) or (shape(mat1)[0] < tolN):
+        return None, leafType(dataSet)
+    return bestIndex, bestValue
+    
