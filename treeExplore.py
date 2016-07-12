db9e15cbe32ba24a7c0b7edeb13b6424feebf8fe
Jussi Martin <jussi.h.a.martin@gmail.com>
2016-07-12 19:09:24 +0300

    ..

---
+++ treeExplore.py
@@ -0,0 +1,33 @@
+from numpy import *
+from tkinter import *
+import regTrees
+
+def reDraw(tolS, tolN):
+    pass
+
+def drawNewTree():
+    pass
+
+root=Tk()
+
+Label(root, text="Plot Place Holder").grid(row=0, columnspan=3)
+
+Label(root, text="tolN").grid(row=1, column=0)
+tolNentry = Entry(root)
+tolNentry.grid(row=1, column=1)
+tolNentry.insert(0,'10')
+Label(root, text="tolS").grid(row=2, column=0)
+tolSentry = Entry(root)
+tolSentry.grid(row=2, column=1)
+tolSentry.insert(0,'1.0')
+Button(root, text="reDraw", command=drawNewTree).grid(row=1, column=2, rowspan=3)
+chkBtnVar = intVar()
+chkBtn = Checkbutton(root, text="Model Tree", variable = chkBtnVar)
+chkBtn.grid(row=3, column=0, columnspan=2)
+
+reDraw.rawDat = mat(regTrees.loadDataSet('cart/sine.txt'))
+reDraw.testDat = arange(min(reDraw.rawDat[:,0]),
+                    max(reDraw.rawDat[:,0]),0.01)
+reDraw(1.0, 10)
+
+root.mainloop()
