1b6ca4c7112452e8a369ebf4d694b38d4fa2571f
Jussi Martin <jussi.h.a.martin@gmail.com>
2016-07-12 17:55:29 +0300

    ..

--- regTrees.py
+++ regTrees.py
@@ -105,3 +105,33 @@ def modelErr(dataSet):
     ws, X, Y = linearSolve(dataSet)
     yHat = X * ws
     return sum(power(Y - yHat, 2))
+
+def regTreeEval(model, inDat):
+    return foat(model)
+
+def modelTreeEval(model, inDat):
+    n = shape(inDat)[1]
+    X = mat(ones((1,n+1)))
+    X[:,1:n+1]=inDat
+    return float(X*model)
+
+def treeForeCast(tree, inData, modelEval=regTreeEval):
+    if not isTree(tree): return modelEval(tree, inData)
+    if inData[tree['spInd']] > tree['spVal']:
+        if isTree(tree['left']):
+            return treeForeCast(tree['left'], inData, modelEval)
+        else:
+            return modelEval(tree['left'], inData)
+    else:
+        if isTree(tree['right']):
+            return treeForeCast(tree['right'], inData, modelEval)
+        else:
+            return modelEval(tree['right'], inData, modelEval)
+
+def createForeCast(tree, testData, modelEval=regTreeEval):
+    m=len(testData)
+    yHat = zeros((m, 1))
+    for i in range(m):
+        yHat[i,0] = treeForeCast(tree, mat(testData[i]), modelEval)
+    return yHat
+    
