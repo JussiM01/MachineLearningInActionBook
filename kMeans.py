442229bacc68ec1e32f506dd2d1e69f99fb0e2c4
Jussi Martin <jussi.h.a.martin@gmail.com>
2016-07-13 18:51:45 +0300

    ..

--- kMeans.py
+++ kMeans.py
@@ -1,4 +1,6 @@
 from numpy import *
+import matplotlib
+import matplotlib.pyplot as plt
 
 def loadDataSet(fileName):
     dataMat = []
@@ -82,4 +84,29 @@ def distSLC(vecA, vecB):
     b = cos(vecA[0,1]*pi/180) * cos(vecB[0,1]*pi/180) * \
                 cos(pi * (vecB[0,0]-vecA[0,0]) /180)
     return arccos(a + b)*6371.0
+
+def clusterClubs(numClust=5):
+    datList = []
+    for line in open('kmeans/places.txt').readlines():
+        lineArr = line.split('\t')
+        datList.append([float(lineArr[4]), float(lineArr[3])])
+    dataMat = mat(datList)
+    myCentroids, clustAssing = biKmeans(datMat, numClust, distMeas=distSLC)
+    fig = plt.figure()
+    rect=[0.1,0.1,0.8,0.8]
+    scatterMarkers=['s', 'o', '^', '8', 'p', 'd', 'v', 'h', '>', '<']
+    axprops = dict(xticks=[], yticks=[])
+    ax0=fig.add_axes(rect, label='ax0', **axprops)
+    imgP = plt.imread('Portland.png')
+    ax0.imshow(imgP)
+    ax1=fig.add_axes(rect, label='ax1', frameon=False)
+    for i in range(numClust):
+        ptsInCurrCluster = datMat[nonzero(clustAssing[:,0].A==i)[0],:]
+        markerStyle = scatterMarkers[i % len(scatterMarkers)]
+        ax1.scatter(ptsInCurrCluster[:,0].flatten().A[0],\
+                    ptsInCurrCluster[:,1].flatten().A[0],\
+                    marker=markerStyle, s=90)
+    ax1.scatter(myCentroids[:,0].flatten().A[0],
+                myCentroids[:,1].flatten().A[0], marker='+', s=300)
+    plt.show()
     
