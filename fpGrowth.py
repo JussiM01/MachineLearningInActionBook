c2f9737f13bb84641566cac685c1129031ecf4f4
Jussi Martin <jussi.h.a.martin@gmail.com>
2016-07-15 16:38:30 +0300

    ..

--- fpGrowth.py
+++ fpGrowth.py
@@ -55,6 +55,15 @@ def updateHeader(nodeToTest, targetNode):
         nodeToTest = nodeToTest.nodeLink
     nodeToTest.nodeLink = targetNode
 
+def loadSimpDat():
+    simpDat = [['r', 'z', 'h', 'j', 'p'],
+               ['z', 'y', 'x', 'w', 'v', 'u', 't', 's'],
+               ['z'],
+               ['r', 'x', 'n', 'o', 's'],
+               ['y', 'r', 'x', 'z', 'q', 't', 'p'],
+               ['y', 'z', 'x', 'e', 'q', 's', 't', 'm']]
+    return simpDat
+
 def createInitSet(dataSet):
     retDict = {}
     for trans in dataSet:
