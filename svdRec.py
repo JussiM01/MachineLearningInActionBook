5562c4a8e9034c330355945d5d3d633f36e293d2
Jussi Martin <jussi.h.a.martin@gmail.com>
2016-07-16 18:11:40 +0300

    ..

--- svdRec.py
+++ svdRec.py
@@ -21,3 +21,27 @@ def cosSim(inA, inB):
     num = float(inA.T*inB)
     denom = la.norm(inA)*la.norm(inB)
     return 0.5 + 0.5 * (num/denom)
+
+def standEst(dataMat, user, simMeas, item):
+    n = shape(dataMat)[1]
+    simTotal = 0.0; ratSimTotal = 0.0
+    for j in range(n):
+        userRating = dataMat[user, j]
+        if userRating == 0: continue
+        overLap = nonzero(logical_and(dataMat[:, item].A>0, dataMat[:, j].A>0))[0]
+        if len(overLap) == 0: similarity = 0
+        else: similarity = simMeas(dataMat[overLap, item], dataMat[overLap, j])
+        #print('the %d and %d similarity is: %f' % (item, j , similarity))
+        simTotal += similarity
+        ratSimTotal += similarity * userRating
+    if simTotal == 0: return 0
+    else: return ratSimTotal/simTotal
+
+def recommend(dataMat, user, N=3, simMeas=cosSim, estMethod=standEst):
+    unratedItems = nonzero(dataMat[user,:].A==0)[1]
+    if len(unratedItems) == 0: return 'you rated everything'
+    itemScores = []
+    for item in unratedItems:
+        estimatedScore = estMethod(dataMat, user, simMeas, item)
+        itemScores.append((item, estimatedScore))
+    return sorted(itemScores, key=lambda jj: jj[1], reverse=True)[:N]
