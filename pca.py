75bd81525c924813489b1afa38656dbd9fbdf9de
Jussi Martin <jussi.h.a.martin@gmail.com>
2016-07-16 16:42:09 +0300

    ..

--- pca.py
+++ pca.py
@@ -2,7 +2,7 @@ from numpy import *
 
 def loadDataSet(fileName, delim='\t'):
     fr = open(fileName)
-    stringArr = [line.strip(delim) for line in fr.readlines()]
+    stringArr = [line.strip().split(delim) for line in fr.readlines()]
     detArr = [list(map(float, line)) for line in stringArr]
     return mat(datArr)
 
