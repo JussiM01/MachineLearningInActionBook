f6f9e1a0d94486c54eda39e12d5475e8236a05b3
Jussi Martin <jussi.h.a.martin@gmail.com>
2016-07-16 16:34:18 +0300

    ..

---
+++ pca.py
@@ -0,0 +1,19 @@
+from numpy import *
+
+def loadDataSet(fileName, delim='\t'):
+    fr = open(fileName)
+    stringArr = [line.strip(delim) for line in fr.readlines()]
+    detArr = [list(map(loat, line)) for line in stringArr]
+    return mat(datArr)
+
+def pca(dataMat, topNFeat=9999999):
+    meanVals = mean(dataMat, axis=0)
+    meanRemoved = dataMat - meanVals
+    covMat = cov(meanRemoved, rowvar=0)
+    eigVals, eigVects = linalg.eig(mat(covMat))
+    eigValInd = argsort(eigVals)
+    eigValInd = eigValInd[:-(topNFeat+1):-1]
+    redEigVects = eigVects[:,eigValInd]
+    lowDDataMat = meanRemoved * redEigVects
+    reconMat = (lowDDataMat * redEigVects.T) + meanVals
+    return lowDDataMat, reconMat
