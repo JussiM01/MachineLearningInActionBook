b82485421a0c97c9083aceae15bf057d24f30cbe
Jussi Martin <jussi.h.a.martin@gmail.com>
2016-07-17 18:00:43 +0300

    ..

--- svdRec.py
+++ svdRec.py
@@ -85,3 +85,29 @@ def svdEst(dataMat, user, simMeas, item):
         ratSimTotal += similarity * userRating
     if simTotal == 0: return 0
     else: return ratSimTotal/simTotal
+
+def printMat(inMat, thresh=0.8):
+    for i in range(32):
+        for k in range(32):
+            if float(inMat[i,k]) > thresh:
+                print('1,')
+            else: prin('0,')
+        print('')
+
+def imgCompress(numSV=3, thresh=0.8):
+    myl = []
+    for line in open('0_5.txt').readlines():
+        newRow = []
+        for i in range(32):
+            newRow.append(int(line[i]))
+        myl.append(newRow)
+    myMat = mat(myl)
+    print("****original matrix******")
+    printMat(myMat, thresh)
+    U,Sigma,VT = la.svd(myMat)
+    SigRecon = mat(zeros((numSV, numSV)))
+    for k in range(numSV):
+        SigRecon[k, k] = Sigma[k]
+    reconMat = U[:,:numSV] * SigRecon * VT[:numSV, :]
+    print("****reconstructed matrix using %d singular values******" % numSV)
+    printMat(reconMat, thresh)
